fc3fb56576f0c9d5b0d4aa59bf02042b1a3a6ae5
Alper Donmez <alper.donmez490@gmail.com>
2018-08-27 17:23:20 -0400

    removed url extention

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,9 @@
 $(document).ready(() => {
 
 	$('nav').on("click", "*", (e) => {
+		e.preventDefault();
 		var scrollTo = e.target.href;
-		scrollTo = scrollTo.slice(scrollTo.length-e.target.innerHTML.length-1, scrollTo.length);
+		scrollTo = scrollTo.slice(scrollTo.length - e.target.innerHTML.length - 1, scrollTo.length);
 
 		$(scrollTo).animatescroll({
 			scrollSpeed: 1000
